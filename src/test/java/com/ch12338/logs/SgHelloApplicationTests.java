328b014345c02ee9eda99631ee071943add96351
egggggg <eggg@gmail.com>
2024-09-26 13:36:30 +0900

    first commit

---
+++ src/test/java/com/ch12338/logs/SgHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch12338.logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
